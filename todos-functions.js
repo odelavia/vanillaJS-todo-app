d55aa06e2c91e76715b9f76f276ad03b903797fa
Oliver De La Via <odelavia@gmail.com>
2018-05-08 21:35:02 -0400

    set up to use strict

--- todos-functions.js
+++ todos-functions.js
@@ -1,7 +1,14 @@
+'use strict'
+
 // read existing todos from localStorage
 const getSavedTodos = () => {
     const todosJSON = localStorage.getItem('todos')
-    return todosJSON ? JSON.parse(todosJSON) : []
+
+    try {
+        return todosJSON ? JSON.parse(todosJSON) : []
+    } catch (e) {
+        return []
+    }
 }
 
 //save todos to localStorage
